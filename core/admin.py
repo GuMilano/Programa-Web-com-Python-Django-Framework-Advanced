840920570051c35de5d963aae4cd773ce6e43be5
GuMilano <gustavo.fenex@hotmail.com>
2026-02-25 16:16:18 -0300

    add - informações no banco serviços/equipe/cargo

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Funcionario, Servico
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'criados', 'modificado')
+    list_filter = ('ativo',)
+    search_fields = ('cargo',)
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'criados', 'modificado')
+    list_filter = ('cargo', 'ativo',)
+    search_fields = ('nome',)
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'criados', 'modificado')
+    list_filter = ('ativo',)
+    search_fields = ('servico',)
